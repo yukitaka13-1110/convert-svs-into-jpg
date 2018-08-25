20099ca76da12d12702b4dae6837a179a4173415
yukitaka13-1110 <yukitaka13.1110@gmail.com>
2018-08-25 16:45:17 +0900

    Tentatively version β

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # convert-from-svs-to-jpg
+
+## About
+
+Simply, convert svs format images into jpg format images. 
+  
+   
+
+## Usage
+
+    $ python convert.py
+
+Command line options shown in the following...
+
+    $ python convert.py -i /path/to/input -o /path/to/output -m CPUCORE_NUM
+
+-i: path to the input images  
+-o: path to the output images  
+-m: Number of CPU cores to use for conversion  
+
+You need for more informatin, see some help.  
+
+    $ python convert.py -h
+
+## Requirement
+
+for Ubuntu
+
+    $ sudo apt-get install python-openslide
+    $ sudo pip install openslide-python
+
+## Installation
+
+    $ git clone https://github.com/yukitaka13-1110/convert-from-svs-to-jpg.git
--- main.py
+++ main.py
@@ -25,11 +25,11 @@ def _get_concat_v(img_lst):
     return dst
 
 def convert(data):
-    UNIT_X, UNIT_Y = 10000, 10000
+    UNIT_X, UNIT_Y = 5000, 5000
     try:
         fname, input_dir, output_dir = data
         save_name = fname.split(".")[0] + ".jpg"
-        print(fname)
+        print("Processing : %s"%fname)
         os_obj = OpenSlide(input_dir+"/"+fname)
         w, h = os_obj.dimensions
         w_rep, h_rep = int(w/UNIT_X)+1, int(h/UNIT_Y)+1
@@ -49,41 +49,43 @@ def convert(data):
                 h_lst.append(img)
                 w_start += UNIT_X
             v_lst.append(h_lst)
+            w_size = UNIT_X
             h_start += UNIT_Y
+            w_start = 0
         concat_h = [_get_concat_h(v) for v in v_lst]
-        connect_hv = _get_concat_v(concat_h)
-        img.save(output_dir+"/"+save_name)
+        concat_hv = _get_concat_v(concat_h)
+        concat_hv.save(output_dir+"/"+save_name)
     except:
         print("Can't open image file : %s"%fname)
         traceback.print_exc()
-    return
-
-    
-
+    return    
 
 def main():
-    parser = argparse.ArgumentParser(description='This script is ...')
+    parser = argparse.ArgumentParser(description='This script is ...'
+                                    , formatter_class=argparse.RawTextHelpFormatter)
     parser.add_argument("--input", "-i", default="./input",
-                        help="Directory name where the input image is saved")
+                        help="Directory name where the input image is saved. default='./input'")
     parser.add_argument("--output", "-o", default="./output",
-                        help="Directory name where the converted image is saved")
+                        help="Directory name where the converted image is saved. default='./output'")
+    parser.add_argument("--multi", "-m", type=int, default=2,
+                        help="Number of CPU cores to use for conversion. default=2")
 
     args = parser.parse_args()
-    print("-- Program Started --")
+    print("------- Program Started -------")
 
     try:
-        f_lst = [[f,args.input,args.output] for f in os.listdir(args.input)]
-        p = Pool(multi.cpu_count())
-        print("-- Convert Started --")
+        f_lst = sorted([f for f in os.listdir(args.input) if ".svs" in f])
+        f_lst = [[f,args.input,args.output] for f in f_lst]
+        p = Pool(args.multi)
+        print("------- Convert Started -------")
         p.map(convert, f_lst)
-        print("-- Convert Ended --")
+        print("------- Convert Ended -------")
         p.close()
     except:
         traceback.print_exc()
     
-    print("-- Program Ended --")
+    print("------- Program Ended -------")
     return
-
 
 
 if __name__ == "__main__":
